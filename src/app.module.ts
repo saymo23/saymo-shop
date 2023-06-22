456c7e8bc50b17cc50b15c71b46646ec11a3b699
Daniel_S <daniel@mexiconow.mx>
2023-06-22 17:20:35 -0600

    Clase 121

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,22 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [ ],
+  imports: [
+    ConfigModule.forRoot(),
+
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      database: process.env.DB_NAME,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASS,
+      autoLoadEntities: true, //Automaticamente carga las entidades del TypeOrm
+      synchronize: true //Sincroniza la base de datos en todos los entornos conectados, algo asi como websockets
+    })
+  ],
   controllers: [],
   providers: [],
 })

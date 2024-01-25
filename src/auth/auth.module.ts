d6c780acc81a491d6568d1e623e05d1fffa03743
Daniel Santarriaga <daniel.santarriaga@gmail.com>
2024-01-25 10:09:34 -0600

    Clase 176: jwt strategy

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -5,6 +5,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   controllers: [AuthController],
@@ -16,12 +17,18 @@ import { JwtModule } from '@nestjs/jwt';
       defaultStrategy: 'jwt'
     }),
 
-    // JwtModule.register({
-    //   secret: process.env.JWT_TOKEN,
-    //   signOptions: {
-    //     expiresIn: '2h'
-    //   }
-    // })
+    JwtModule.registerAsync({
+      imports:[ ConfigModule ],
+      inject:[ ConfigService ],
+      useFactory: ( configService: ConfigService ) => {
+        return {
+          secret: configService.get('JWT_TOKEN'),
+          signOptions:{
+            expiresIn: '2h'
+          }
+        }
+      }
+    })
   ],
   exports: [ TypeOrmModule ]
 })
